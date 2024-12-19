a6f5801b0160583e36021f48e65779164d0588d0
John <johnybradshaw@users.noreply.github.com>
2024-12-19 16:55:48 +0000

    Fix hanging "

--- provisioner-scripts/01-apt_mirror_reset.sh
+++ provisioner-scripts/01-apt_mirror_reset.sh
@@ -2,4 +2,4 @@
 rm -rf /var/lib/apt/lists/*
 sed -i 's|http://mirrors.linode.com|http://archive.ubuntu.com|g' /etc/apt/sources.list
 echo 'Acquire::Languages \"none\";' | tee /etc/apt/apt.conf.d/99disable-translations
-apt-get update -o Acquire::CompressionTypes::Order::=gz"
+apt-get update -o Acquire::CompressionTypes::Order::=gz
