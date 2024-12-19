76b3438e866af2d79fd84683adc036c8ed1f3974
John <johnybradshaw@users.noreply.github.com>
2024-12-19 20:01:44 +0000

    dpkg error

--- provisioner-scripts/03-cloud_init.sh
+++ provisioner-scripts/03-cloud_init.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-DEBIAN_FRONTEND=noninteractive
-echo 'Setting debconf to non-interactive mode...' # Set debconf to non-interactive mode
-echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
-apt-get update # Update package lists
-apt-get dist-upgrade -y -o Dpkg::Options::=\"--force-confold\" -o Dpkg::Options::=\"--force-confdef\"
+set -e  # Exit immediately if a command exits with a non-zero status
+export DEBIAN_FRONTEND=noninteractive
+
+echo "Setting debconf to non-interactive mode..."
+echo "debconf debconf/frontend select Noninteractive" | debconf-set-selections
+
+echo "Updating package lists..."
+apt-get update
+
+echo "Performing a distribution upgrade..."
+apt-get dist-upgrade -y \
+    -o Dpkg::Options::='--force-confold' \
+    -o Dpkg::Options::='--force-confdef'
 cloud-init init --local # Initialize cloud-init
 cloud-init modules --mode=config # Generate cloud-init config
 cloud-init modules --mode=final # Generate cloud-init final
